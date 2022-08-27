bcea9cd8fe8f9941da0d2f98d7fade4532162e1e
Bishal Chettri <bistimulus0@gmail.com>
2022-08-27 16:31:57 +0530

    Started

---
+++ python_repos.py
@@ -0,0 +1,17 @@
+import requests
+
+#make an api call and store the response
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url,headers = headers)
+print(f"Status code: {r.status_code}")
+
+
+#then store API response in a variable
+response_dict = r.json()
+
+#process results
+print(response_dict.keys())
+
+
