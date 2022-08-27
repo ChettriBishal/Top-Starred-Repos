1fde9a06bc2915890668d30f15fbd8b1ab07a594
Bishal Chettri <bistimulus0@gmail.com>
2022-08-27 17:00:08 +0530

    Testing

--- python_repos.py
+++ python_repos.py
@@ -11,7 +11,24 @@ print(f"Status code: {r.status_code}")
 #then store API response in a variable
 response_dict = r.json()
 
+
+print(f"Total repositories: {response_dict['total_count']}")
+
+#getting more information about the repositories
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(response_dict)}")
+
+#examining the first repository
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+
+for key in sorted(repo_dict):
+    print(key)
+
+
 #process results
 print(response_dict.keys())
+
+
 
 
